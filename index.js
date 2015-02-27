2a84d8dc1312c763dbba2d7431bf2df5611eaf4f
mareenator <mareenator@mareenator-Lenovo-G510.(none)>
2015-02-27 18:01:42 +0530

    Furst

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express'),base_url='http://localhost:3000/';
+var app = express(),
+    exphbs  = require('express-handlebars');
+var request = require('request');
+var http = require('http').Server(app);
+var bodyParser = require('body-parser');
+var util = require('util'),
+    EventEmitter = require('events').EventEmitter;
+
+
+app.engine('.hbs', exphbs({defaultLayout: 'main',extname:'.hbs'}));
+app.set('view engine', '.hbs'); 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/public", express.static(__dirname + "/public"));
+
+//Basic application getting loaded : The web interface.
+app.get('/',function(req,res){
+    res.render('home', { data : { base_url : base_url } } );     
+});
+
+http.listen(3000,function(){
+    console.log('Listening on *:3000');
+});
